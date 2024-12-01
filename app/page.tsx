dfd59cdfaabac2c8e6c007f14150fe6c85b980d6
gavinongmh <gavin.ongmh@gmail.com>
2024-12-01 23:26:08 +1300

    Tailwind CSS Setup and theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl font-black text-violet-700">Hello world!</h1>;
+  return <h1 className="text-3xl font-black text-light-500">Hello world!</h1>;
 }
