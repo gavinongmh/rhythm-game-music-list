5c96c01524ac5ce840dfcc890f878641a8f47d70
gavinongmh <gavin.ongmh@gmail.com>
2024-12-01 22:45:57 +1300

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl text-violet-700 font-black">Hello world!</h1>;
+  return <h1 className="text-3xl font-black text-violet-700">Hello world!</h1>;
 }
