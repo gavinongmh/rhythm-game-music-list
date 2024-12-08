95f24acfbe8b43cf8c8e6d731805d7ed48e6dac9
gavinongmh <gavin.ongmh@gmail.com>
2024-12-09 00:22:02 +1300

    Implemented Error Handling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import handleError from "@/lib/handlers/error";
 const posts = [
   {
     _id: "1",
