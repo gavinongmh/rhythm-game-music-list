20cad38e9be2f5dfdad04402850720f78dd1607f
gavinongmh <gavin.ongmh@gmail.com>
2024-12-10 16:18:21 +1300

    Implemented Password/Email auth

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,7 +2,8 @@
 import React from "react";
 
 import AuthForm from "@/components/forms/AuthForm";
-import { SignInSchema, SignUpSchema } from "@/lib/validations";
+import { signUpWithCredentials } from "@/lib/actions/auth.action";
+import { SignUpSchema } from "@/lib/validations";
 
 const SignUp = () => {
   return (
@@ -10,7 +11,7 @@ const SignUp = () => {
       formType="SIGN_UP"
       schema={SignUpSchema}
       defaultValues={{ email: "", password: "", name: "", username: "" }}
-      onSubmit={(data) => Promise.resolve({ success: true, data })}
+      onSubmit={signUpWithCredentials}
     />
   );
 };
--- auth.ts
+++ auth.ts
@@ -1,16 +1,59 @@
+import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-import { api } from "./lib/api";
 import { IAccountDoc } from "./database/account.model";
+import { IUserDoc } from "./database/user.model";
+import { api } from "./lib/api";
+import { SignInSchema } from "./lib/validations";
 
 // We'll check if the sign-in account type is credentials; if yes, then we skip. We'll handle it the other way around when doing email password-based authentication.
 
 // But if the account type is not credentials, we'll call this new "signin-with-oauth" api and create oAuth accounts.
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [
+    GitHub,
+    Google,
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = SignInSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const { data: existingAccount } = (await api.accounts.getByProvider(
+            email
+          )) as ActionResponse<IAccountDoc>;
+
+          if (!existingAccount) return null;
+
+          const { data: existingUser } = (await api.users.getById(
+            existingAccount.userId.toString()
+          )) as ActionResponse<IUserDoc>;
+
+          if (!existingUser) return null;
+
+          const isValidPassword = await bcrypt.compare(
+            password,
+            existingAccount.password!
+          );
+
+          if (isValidPassword) {
+            return {
+              id: existingUser.id,
+              name: existingUser.name,
+              email: existingUser.email,
+              image: existingUser.image,
+            };
+          }
+        }
+        return null;
+      },
+    }),
+  ],
   callbacks: {
     async session({ session, token }) {
       session.user.id = token.sub as string;
